732592b1ce3594072ad3040f149be49d2a5e911d
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-12-13 20:30:10 +0900

    feat: 입력값이 서로 다른 3자리의 수가 아닌 경우 예외 처리

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,22 @@
-const Inputview = {};
+const { Console } = require('@woowacourse/mission-utils');
 
-module.exports = Inputview;
+const InputView = {
+  COMMAND: '숫자를 입력해주세요',
+  LENGTH: 3,
+  readUserNumbers() {
+    Console.readLine(InputView.COMMAND, answer => {
+      const userNumbers = Array.from(answer);
+      InputView.validateUserNumbers(userNumbers);
+      Console.print(userNumbers);
+    });
+  },
+  validateUserNumbers(userNumbers) {
+    if (userNumbers.length !== InputView.LENGTH) throw new Error();
+    if (!userNumbers.every(number => number > 0 && number < 10)) throw new Error();
+    if (userNumbers.length !== new Set(userNumbers).size) throw new Error();
+  },
+};
+
+InputView.readUserNumbers();
+
+module.exports = InputView;
