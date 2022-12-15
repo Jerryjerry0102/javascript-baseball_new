854c5eea37b408e4f0e7d6133d7da84a955a5888
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-12-15 15:56:26 +0900

    feat: 게임 결과를 출력하는 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -5,6 +5,16 @@ const OutputView = {
   printStartMessage() {
     Console.print(OutputView.START_MESSAGE);
   },
+
+  printResult(strikeCount, ballCount) {
+    if (strikeCount === 3) {
+      Console.print('3스트라이크');
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    } else if (strikeCount === 0 && ballCount === 0) Console.print('낫싱');
+    else if (strikeCount === 0) Console.print(`${ballCount}볼`);
+    else if (ballCount === 0) Console.print(`${strikeCount}스트라이크`);
+    else Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+  },
 };
 
 module.exports = OutputView;
