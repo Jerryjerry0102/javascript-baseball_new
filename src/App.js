960c01b8d9d4b2cc6443b95d6f20de87c5b641c4
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-12-15 15:21:03 +0900

    feat: 기능 구현한 InputView, NumbersGenerator, NumbersComparator를 controller(App.js)에서 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
+const NumbersGenerator = require('./NumbersGenerator');
+const NumbersComparator = require('./NumbersComparator');
 
 class App {
-  play() {
-    App.startBaseballGame();
+  constructor() {
+    this.numbersComparator;
   }
 
-  static startBaseballGame() {
+  play() {
+    this.startBaseballGame();
+  }
+
+  startBaseballGame() {
     OutputView.printStartMessage();
+    const computerNumbers = NumbersGenerator.generate();
+    console.log(computerNumbers); // 삭제
+    this.numbersComparator = new NumbersComparator(computerNumbers);
+    this.controlUserNumbers();
+  }
+
+  controlUserNumbers() {
+    InputView.readUserNumbers(userNumbers => {
+      this.controlBaseballGame(userNumbers);
+    });
+  }
+
+  controlBaseballGame(userNumbers) {
+    const { strikeCount, ballCount } = this.numbersComparator.compare(userNumbers);
+    console.log('count', strikeCount, ballCount); // 삭제
+    if (strikeCount !== 3) this.controlUserNumbers();
   }
 }
 
