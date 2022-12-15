31883c43de5a48a835e4b318da27b986d2e0b73a
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-12-15 17:55:44 +0900

    feat: 입력값이 1 또는 2가 아닌 경우 예외 처리

--- src/InputView.js
+++ src/InputView.js
@@ -3,18 +3,31 @@ const { Console } = require('@woowacourse/mission-utils');
 const InputView = {
   COMMAND: '숫자를 입력해주세요',
   LENGTH: 3,
+  RETRY: 1,
+  END: 2,
+  RETRY_COMMAND: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
   readUserNumbers(callback) {
-    Console.readLine(InputView.COMMAND, answer => {
+    Console.readLine(this.COMMAND, answer => {
       const userNumbers = answer.split('').map(number => Number(number));
-      InputView.validateUserNumbers(userNumbers);
+      this.validateUserNumbers(userNumbers);
       console.log(userNumbers); // 삭제
       callback(userNumbers);
     });
   },
   validateUserNumbers(userNumbers) {
-    if (userNumbers.length !== InputView.LENGTH) throw new Error();
+    if (userNumbers.length !== this.LENGTH) throw new Error();
     if (!userNumbers.every(number => number > 0 && number < 10)) throw new Error();
     if (userNumbers.length !== new Set(userNumbers).size) throw new Error();
+  },
+
+  readRetry(callback) {
+    Console.readLine(this.RETRY_COMMAND, answer => {
+      this.validateRetry(Number(answer));
+      callback(Number(answer));
+    });
+  },
+  validateRetry(answer) {
+    if (!(answer === this.RETRY || answer === this.END)) throw new Error();
   },
 };
 
