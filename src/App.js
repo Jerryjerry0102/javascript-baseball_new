19ee904bb6269097bfeda499ce6928cf555e47b9
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-12-15 17:22:45 +0900

    feat: 재시작/종료를 위한 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+const { Console } = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
 const InputView = require('./InputView');
 const NumbersGenerator = require('./NumbersGenerator');
 const NumbersComparator = require('./NumbersComparator');
 
 class App {
+  #RETRY;
+
   constructor() {
-    this.numbersComparator;
+    this.#RETRY = '1';
   }
 
   play() {
+    OutputView.printStartMessage();
     this.startBaseballGame();
   }
 
   startBaseballGame() {
-    OutputView.printStartMessage();
     const computerNumbers = NumbersGenerator.generate();
     console.log(computerNumbers); // 삭제
     this.numbersComparator = new NumbersComparator(computerNumbers);
@@ -28,8 +31,16 @@ class App {
 
   controlBaseballGame(userNumbers) {
     const { strikeCount, ballCount } = this.numbersComparator.compare(userNumbers);
-    console.log('count', strikeCount, ballCount); // 삭제
+    OutputView.printResult(strikeCount, ballCount);
     if (strikeCount !== 3) this.controlUserNumbers();
+    else this.controlRetry();
+  }
+
+  controlRetry() {
+    InputView.readRetry(answer => {
+      if (answer === this.RETRY) this.startBaseballGame();
+      Console.close();
+    });
   }
 }
 
