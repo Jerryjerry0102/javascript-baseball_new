fdd5e9a2a744775602f3fe66f7fff52cea8167f8
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-12-15 16:14:08 +0900

    refactor: 출력하는 메시지들 상수로 교체

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,18 +2,22 @@ const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
   START_MESSAGE: '숫자 야구 게임을 시작합니다.',
+  END_MESSAGE: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  STRIKE: '스트라이크',
+  BALL: '볼',
+  NOTHING: '낫싱',
   printStartMessage() {
     Console.print(OutputView.START_MESSAGE);
   },
 
   printResult(strikeCount, ballCount) {
     if (strikeCount === 3) {
-      Console.print('3스트라이크');
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    } else if (strikeCount === 0 && ballCount === 0) Console.print('낫싱');
-    else if (strikeCount === 0) Console.print(`${ballCount}볼`);
-    else if (ballCount === 0) Console.print(`${strikeCount}스트라이크`);
-    else Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+      Console.print(strikeCount + OutputView.STRIKE);
+      Console.print(OutputView.END_MESSAGE);
+    } else if (strikeCount === 0 && ballCount === 0) Console.print(OutputView.NOTHING);
+    else if (strikeCount === 0) Console.print(ballCount + OutputView.BALL);
+    else if (ballCount === 0) Console.print(strikeCount + OutputView.STRIKE);
+    else Console.print(ballCount + OutputView.BALL + ' ' + strikeCount + OutputView.STRIKE);
   },
 };
 
